c66529c4257e44e222e50142250cfd3d0447b901
1033141032 <1033141032@qq.com>
2021-04-04 19:48:28 +0800

    updated

--- example/main.go
+++ example/main.go
@@ -7,14 +7,16 @@ func main() {
 	if err != nil {
 		zztlog.Debug(err)
 	}
-	zztlog.Info(123456)
-	zztlog.InfoF("%s+%s", "我", "你")
-	zztlog.Debug(123456)
-	zztlog.DebugF("%s+%s", "我", "你")
-	zztlog.Error(123456)
-	zztlog.ErrorF("%s+%s", "我", "你")
-	zztlog.Fatal(123456)
-	zztlog.FatalF("%s+%s", "我", "你")
-	zztlog.Warn(123456)
-	zztlog.WarnF("%s+%s", "我", "你")
+	for {
+		zztlog.Info(123456)
+		zztlog.InfoF("%s+%s", "我", "你")
+		zztlog.Debug(123456)
+		zztlog.DebugF("%s+%s", "我", "你")
+		zztlog.Error(123456)
+		zztlog.ErrorF("%s+%s", "我", "你")
+		zztlog.Fatal(123456)
+		zztlog.FatalF("%s+%s", "我", "你")
+		zztlog.Warn(123456)
+		zztlog.WarnF("%s+%s", "我", "你")
+	}
 }
